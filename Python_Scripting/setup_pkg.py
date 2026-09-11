866aba4448f0b40ac3d81430b7a345c348050054
S M Tahmeed Reza <tahmeedreza@gmail.com>
2026-09-11 23:32:18 +0600

    Issue fixed: image reconstruction is happening correctly. Reason: download arc2face.onnx, delete glintr100.onnx. Found solution on github repo
    Changes to be committed:
    	modified:   .gitignore
    	modified:   Python_Scripting/setup_pkg.py

--- Python_Scripting/setup_pkg.py
+++ Python_Scripting/setup_pkg.py
@@ -26,3 +26,5 @@ hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="arc2face/config.json", l
 hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="arc2face/diffusion_pytorch_model.safetensors", local_dir="./models")
 hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="encoder/config.json", local_dir="./models")
 hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="encoder/pytorch_model.bin", local_dir="./models")
+
+hf_hub_download(repo_id="FoivosPar/Arc2Face", filename="arcface.onnx", local_dir="./models/antelopev2")
